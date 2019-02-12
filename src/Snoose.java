10b3886caba0ac92cc7b4d0fcfe76a6040b9431e
HeyItzGinger <league@iMac-2.attlocal.net>
2019-02-11 16:56:19 -0800

    Snoose

--- src/Snoose.java
+++ src/Snoose.java
@@ -1,9 +1,20 @@
+import javax.swing.JFrame;
 
 public class Snoose {
-
+JFrame frame;
+int width = 1000;
+int height = 800;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Snoose().setup();
+	}
+	public Snoose() {
+		frame = new JFrame();
+	}
+	public void setup() {
+		frame.setSize(width, height);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
