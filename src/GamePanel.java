3258c5b3fa159d69eea8cd8d1c876ff696eff604
Sasanka742 <league@iMac-2.attlocal.net>
2019-03-04 16:20:46 -0800

    snooose

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,13 +1,23 @@
 import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
-
+public class GamePanel extends JPanel implements KeyListener {
+	int state = 0;
+	JPanel menuPanel = new JPanel();
+	JPanel gamePanel = new JPanel();
+	JPanel endPanel = new JPanel();
 	public GamePanel() {
 		Snoose.frame.setVisible(true);
 		Snoose.frame.setSize(Snoose.width, Snoose.height);
 		drawMenuState();
+		menuPanel.addKeyListener(this);
+		gamePanel.addKeyListener(this);
+		endPanel.addKeyListener(this);
+		Snoose.frame.add(menuPanel);
 	}
 
 	public void updateMenuState() {
@@ -23,23 +33,58 @@ public class GamePanel extends JPanel {
 	}
 
 	public void drawMenuState() {
-		JPanel menuPanel = new JPanel();
 		menuPanel.setSize(Snoose.width, Snoose.height);
 		menuPanel.setBackground(new Color(255, 238, 170));
-		Snoose.frame.add(menuPanel);
 	}
 
 	public void drawGameState() {
-		JPanel gamePanel = new JPanel();
 		gamePanel.setSize(Snoose.width, Snoose.height);
 		gamePanel.setBackground(new Color(187, 187, 255));
 		Snoose.frame.add(gamePanel);
 	}
 
 	public void drawEndState() {
-		JPanel endPanel = new JPanel();
 		endPanel.setSize(Snoose.width, Snoose.height);
 		endPanel.setBackground(new Color(255, 238, 170));
 		Snoose.frame.add(endPanel);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		if (state == 0) {
+			drawMenuState();
+		}
+		else if (state == 1) {
+			drawGameState();
+		} 
+		else if (state == 2) {
+			drawEndState();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_ENTER && state == 0) {
+			Snoose.frame.getContentPane().removeAll();
+			state = 1;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_ENTER && state == 1) {
+			Snoose.frame.getContentPane().removeAll();
+			state = 2;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_ENTER && state == 2) {
+			Snoose.frame.getContentPane().removeAll();
+			state = 0;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
